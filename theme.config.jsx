c011b8921bfe61091d782c2c3ad92334d347e830
Alexandre Betioli <apbetioli@gmail.com>
2024-04-11 15:30:29 +0200

    Add nextra

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>MPM2D Documentation</span>,
+  project: {
+    link: "https://github.com/apbetioli/mpm2d-docs",
+  },
+  docsRepositoryBase: "https://github.com/apbetioli/mpm2d-docs/tree/main/docs",
+};
