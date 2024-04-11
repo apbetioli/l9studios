fa2ef18585e738c50e856d897dc3f920dafa37ba
Alexandre Betioli <apbetioli@gmail.com>
2024-04-11 17:19:41 +0200

    Theme fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,32 @@
 "use client";
-import { BackgroundBeams } from "@/components/background-beams";
-import React from "react";
-
-export default function BackgroundBeamsDemo() {
+import { TypewriterEffectSmooth } from "@/components/typewriter-effect";
+export default function TypewriterEffectSmoothDemo() {
+  const words = [
+    {
+      text: "Easily",
+    },
+    {
+      text: "create",
+    },
+    {
+      text: "2D Moving Platforms!",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
   return (
-    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Join the waitlist
-        </h1>
-        <p></p>
-        <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-          Welcome to MailJet, the best transactional email service on the web.
-          We provide reliable, scalable, and customizable email solutions for
-          your business. Whether you&apos;re sending order confirmations,
-          password reset emails, or promotional campaigns, MailJet has got you
-          covered.
-        </p>
-        <input
-          type="text"
-          placeholder="hi@manuarora.in"
-          className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-        />
+    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        The road to freedom starts from here
+      </p>
+      <TypewriterEffectSmooth words={words} />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+        <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
+          Join now
+        </button>
+        <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
+          Signup
+        </button>
       </div>
-      <BackgroundBeams />
     </div>
   );
 }
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,17 @@
-export default {
-  logo: <span>MPM2D Documentation</span>,
+const Theme = {
+  logo: <span>Moving Platform Maker 2D</span>,
   project: {
     link: "https://github.com/apbetioli/mpm2d-docs",
   },
   docsRepositoryBase: "https://github.com/apbetioli/mpm2d-docs/tree/main/docs",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – MPM2D",
+    };
+  },
+  footer: {
+    component: false,
+  },
 };
+
+export default Theme;
