07238968558e5918930a1d9f91e181a08f778493
Alexandre Betioli <apbetioli@gmail.com>
2024-04-15 16:21:40 +0200

    Change headline

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <div className="flex flex-col items-center justify-center bg-white dark:bg-black pt-10 gap-10">
       <h1 className="text-neutral-600 dark:text-neutral-200 text-4xl md:text-8xl font-bold text-center">
-        2D moving platforms made ease
+        Easily create 2D moving platforms
       </h1>
 
       <Link href="https://assetstore.unity.com/packages/tools/level-design/moving-platform-maker-2d-71886?utm-source=lp">
